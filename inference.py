3c69885c1e107a5d1931756da2fc8019eeef4d0e
Paolo Arnaldo Dallari <paolo.arnaldo@gmail.com>
2022-05-22 10:57:54 +0200

    little mods and semplifications

--- inference.py
+++ inference.py
@@ -1,14 +1,10 @@
 import torch
 import cv2
 import torchvision.transforms as transforms
-import argparse
 from model import CNNModel
-# construct the argument parser
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', 
-    default='input/test/good/out-10996.jpg',
-    help='path to the input image')
-args = vars(parser.parse_args())
+
+#Path of image in test
+path='input/test_image.jpg'
 
 # the computation device
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
@@ -25,7 +21,7 @@ model.eval()
 # define preprocess transforms
 transform = transforms.Compose([
     transforms.ToPILImage(),
-    transforms.Resize(224),
+    transforms.Resize((224,224)),
     transforms.ToTensor(),
     transforms.Normalize(
         mean=[0.5, 0.5, 0.5],
@@ -33,11 +29,7 @@ transform = transforms.Compose([
     )
 ])  
 
-
-image = cv2.imread(args['input'])
-# get the ground truth class
-gt_class = args['input'].split('/')[-2]
-orig_image = image.copy()
+image = cv2.imread(path)
 # convert to RGB format
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image = transform(image)
@@ -47,5 +39,5 @@ with torch.no_grad():
     outputs = model(image.to(device))
 output_label = torch.topk(outputs, 1)
 pred_class = labels[int(output_label.indices)]
-print(f"GT: {gt_class}, pred: {pred_class}")
+print(f"Prediction: {pred_class} camera")
     
--- input/train/bad/test.sh
+++
@@ -1,3 +0,0 @@
-for x in `seq 1 100`; do
-    cp test_image.jpg $(printf "test_image-%03d.jpg" $x)
-done
